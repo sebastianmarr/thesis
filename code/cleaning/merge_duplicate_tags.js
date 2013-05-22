0af8779d28df4a5804652979667f679f0e7df922
Sebastian Marr <marr@spreadshirt.net>
2013-05-22 15:45:28 +0200

    improvement on tag-merging script

--- code/cleaning/merge_duplicate_tags.js
+++ code/cleaning/merge_duplicate_tags.js
@@ -1,34 +1,56 @@
-// look for duplicate tags (just different case) and delete them after 
+// look for duplicate tags (after sanitizing) and delete them after 
 // replacing all references to them with the canonical one
 
+// MR to count occurences per tag in language
+var map = function() {
+    emit({tag: this.tag, lang: this.lang}, 1)
+}
+var reduce = function(key, values) {
+    return Array.sum(values)
+}
+db.ss_tags.mapReduce(map, reduce, {out: "mr_duplicate_tags"})
+
+// remove duplicates by merging tags
+db.ss_tags.ensureIndex({tag:1, lang:1})
+db.ss_tag_link_object.ensureIndex({tag_id: true})
+db.mr_duplicate_tags.find({value:{$gt:1}}).toArray().forEach(function(dups) {
+
+    var tags = db.ss_tags.find(dups._id).toArray()
+    var canonical = tags[0]
+    for (var i = tags.length - 1; i >= 1; i--) {
+        var dup = tags[i]
+        db.ss_tag_link_object.update(
+            {tag_id: dup.tag_id},
+            {$set: {tag_id: canonical.tag_id}},
+            {multi: true}
+        )
+        db.ss_tags.remove(dup._id)
+    }
+})
+db.mr_duplicate_tags.drop()
+
+// remove duplicate link entries after merging tags
+var map = function() {
+    emit({tag_id: this.tag_id, object_id: this.object_id, object_type_id: this.object_type_id}, 1)
+}
+var reduce = function(key, values) {
+    return Array.sum(values)
+}
+db.ss_tag_link_object.mapReduce(map, reduce, {out: "mr_duplicate_tag_links"})
+
+db.ss_tag_link_object.ensureIndex({tag_id:1, object_id:1, object_type_id:1})
+db.mr_duplicate_tag_links.find({value:{$gt:1}}).toArray().forEach(function(dups) {
+    var links = db.ss_tag_link_object.find(dups._id).toArray()
+    for (var i = links.length - 1; i >= 1; i--) {
+        var dup = links[i]
+        db.ss_tag_link_object.remove(dup._id)
+    };
+})
+db.mr_duplicate_tag_links.drop()
+
 // remove all tags that are never referenced
-db.ss_tag_link_object.ensureIndex({"tag_id": true})
 db.ss_tags.find().toArray().forEach(function(tag) {
     if (db.ss_tag_link_object.count({"tag_id": tag.tag_id}) == 0) {
         db.ss_tags.remove({_id: tag._id})
     }
 })
-
-// merge duplicate tags
-db.ss_tags.ensureIndex({"tag": true, "lang": true})
-db.ss_tags.ensureIndex({"tag_id": true})
-db.ss_tags.find().addOption(DBQuery.Option.noTimeout).forEach(function(tag) {
-    db.ss_tags.find(
-        {"tag": tag.tag, "lang": tag.lang, "tag_id": {$ne: tag.tag_id}}
-    ).forEach(function(duplicate) {
-        db.ss_tag_link_object.update(
-            {"tag_id": duplicate.tag_id},
-            {$set: {"tag_id": tag.tag_id}},                
-            {multi: true}
-        )
-        db.ss_tags.remove({tag_id: duplicate.tag_id})
-    })
-})
-
-// remove duplicates in ss_tag_link_object by creating a unique composite index on
-// tag_id, object_id and object_type_id,
-// enforcing the build of the index by dropping duplicates
-db.ss_tag_link_object.ensureIndex(
-    {"tag_id": 1, "object_id": 1, "object_type_id": 1},
-    {unique: true, dropDups: true}
-) 
