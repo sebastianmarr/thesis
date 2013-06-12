2c31fae6233accc967d46aa8780f20263b9ac228
Sebastian Marr <marr@spreadshirt.net>
2013-06-12 10:41:08 +0200

    graph subset extraction: small changes

--- code/graph/extract_subset.js
+++ code/graph/extract_subset.js
@@ -1,13 +1,16 @@
-// this extraxts all german tags that have at least one sale
+db.tags.drop();
+db.links.drop();
 
-db.tags.drop()
-db.links.drop()
+db.ss_tags.ensureIndex({tag_id: 1});
+db.ss_tag_link_object.ensureIndex({tag_id: 1});
 
-db.mr_orders_per_tag.ensureIndex({"tag.lang": 1})
-db.mr_orders_per_tag.find({"tag.lang": "de"}).addOption(DBQuery.Option.noTimeout).forEach(function(tag) {
+db.ss_tag_link_object.remove({tag_id: 0})
+db.mr_orders_per_tag.remove({_id: 0})
 
-    db.tags.insert(tag.tag)
-    db.ss_tag_link_object.find({"tag_id": tag._id}).forEach(function(link) {
-        db.links.insert(link)
-    })
-})
+db.mr_orders_per_tag.find().addOption(DBQuery.Option.noTimeout).forEach(function(mr) {
+    var tag = db.ss_tags.findOne({tag_id: mr._id});
+    db.tags.insert(tag);
+    db.ss_tag_link_object.find({tag_id: tag.tag_id}).forEach(function(link) {
+        db.links.insert(link);
+    });
+});
