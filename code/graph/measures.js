826b1191b769008b501c73c7557797a2ad67779f
Sebastian Marr <marr@spreadshirt.net>
2013-06-12 10:39:59 +0200

    remove overlap measure

--- code/graph/measures.js
+++ code/graph/measures.js
@@ -6,7 +6,6 @@ db.tag_graph_edges.find().addOption(DBQuery.Option.noTimeout).forEach(function(e
         var target_occs = db.tag_graph_nodes.findOne({_id: n.id},{occs: 1}).occs
         n.dice = 2 * n.occs / (node_occs + target_occs)
         n.jaccard = n.occs / (node_occs + target_occs - n.occs)
-        n.overlap = n.occs / (Math.min(node_occs, target_occs))
         n.ochiai = n.occs / (Math.sqrt(node_occs * target_occs))
     })
     db.tag_graph_edges_measured.save(edge)
