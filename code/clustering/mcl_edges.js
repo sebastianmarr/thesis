d376b133e133c93c9aae90dfd954d40f9b9af776
Sebastian Marr <marr@spreadshirt.net>
2013-06-17 15:10:21 +0200

    fix mcl script

--- code/clustering/mcl_edges.js
+++ code/clustering/mcl_edges.js
@@ -5,12 +5,12 @@ db.mcl_edges.ensureIndex({a: 1, b: 1})
 db.tag_graph_edges.find().addOption(DBQuery.Option.noTimeout).forEach(function(edge) {
     var a = db.tag_graph_nodes.findOne({_id: edge._id}).tag
     edge.tn.forEach(function(t) {
-        var b = db.tag_graph_nodes.findOne({_id: t._id}).tag
+        var b = db.tag_graph_nodes.findOne({_id: t.id}).tag
         if (db.mcl_edges.count({$or: [{a: a, b: b}, {a: b, b: a}]}) == 0) {
             db.mcl_edges.insert({
                 a: a,
                 b: b,
-                c: t.dice
+                c: t.ochiai
             })
         }
     })
