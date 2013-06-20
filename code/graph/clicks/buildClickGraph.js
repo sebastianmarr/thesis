e14429ba0256d97429431e9e82d516d383b998b1
Sebastian Marr <marr@spreadshirt.net>
2013-06-20 14:31:11 +0200

    add diagnostic messages to graph building

--- code/graph/clicks/buildClickGraph.js
+++ code/graph/clicks/buildClickGraph.js
@@ -1,4 +1,8 @@
+print("Cleanup");
 load('cleanup.js');
+print("Nodes");
 load('nodes.js');
+print("Edges");
 load('edges.js');
+print("Measures");
 load('measures.js');
--- code/graph/tags/buildTagGraph.js
+++ code/graph/tags/buildTagGraph.js
@@ -1,4 +1,8 @@
-load('extractSubset.js');
+print("Subset");
+load('extract_subset.js');
+print("Nodes");
 load('nodes.js');
+print("Edges");
 load('edges.js');
+print("Measures");
 load('measures.js');
