26a9534bde75b0d951535d04eeff418be8992e19
Sebastian Marr <marr@spreadshirt.net>
2013-07-02 08:02:58 +0200

    refactor: joining of tags and links

--- code/cleaning/tags/cleanup.js
+++ code/cleaning/tags/cleanup.js
@@ -2,6 +2,10 @@ print("Sanitizing...")
 load('cleaning/tags/sanitize.js');
 print("Done.\n")
 
+print("Joining Tags with Objects...")
+load('cleaning/tags/joinTagsWithLinks.js');
+print("Done.\n")
+
 print("Removing Duplicates...");
 load('cleaning/tags/dedup.js');
 print("Done.\n");
