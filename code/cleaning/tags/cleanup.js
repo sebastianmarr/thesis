8ebccec8009b2c3adfdfdbd6063d71ba766b9f36
Sebastian Marr <marr@spreadshirt.net>
2013-07-02 13:19:25 +0200

    structure changes

--- code/cleaning/tags/cleanup.js
+++ code/cleaning/tags/cleanup.js
@@ -3,17 +3,9 @@ load('cleaning/tags/sanitize.js');
 print("Done.\n")
 
 print("Joining Tags with Objects...")
-load('cleaning/tags/joinTagsWithLinks.js');
+load('cleaning/tags/join.js');
 print("Done.\n")
 
 print("Removing Duplicates...");
 load('cleaning/tags/dedup.js');
-print("Done.\n");
-
-print("Ranking by Orders...");
-load('cleaning/tags/rankByOrders.js');
-print("Done.\n");
-
-print("Extracting Subset...");
-load('cleaning/tags/subset.js');
 print("Done.\n");
