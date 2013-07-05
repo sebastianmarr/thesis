eb1f48ddb8aa28241bacd76f59dc79431d3520c3
Sebastian Marr <marr@spreadshirt.net>
2013-07-05 17:24:01 +0200

    specify mysql import tables

--- code/import/mysql/run_import.rb
+++ code/import/mysql/run_import.rb
@@ -8,11 +8,11 @@ db_configuration = ARGV[0]
 
 i = Importer.new(db_configuration, reset_mongo:true, shard: true)
 
-['ss_tags', 
- 'ss_tag_link_object',
- 'ss_productng_configurations',
- 'ss_order_orderitems'
-].each do |table|
+{'ss_tags' => ['tag_id', 'tag', 'lang'], 
+ 'ss_tag_link_object' => ['tag_id', 'object_id', 'object_type_id'],
+ 'ss_productng_configurations' => ['product_id', 'type', 'design_id'],
+ 'ss_order_orderitems' => ['orderitem_id', 'order_id', 'product_id', 'article_id', 'quantity']
+}.each do |table, columns|
     puts "importing #{table}..."
     i.import(table, shard: true)
 end
