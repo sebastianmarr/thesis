03619f91b8dfa4146b951e7372dc71b09e5b3665
Sebastian Marr <marr@spreadshirt.net>
2013-07-16 09:11:48 +0200

    calculate tag graph measures via M/R

---
+++ code/graph/mr_tag_graph_measures.js
@@ -0,0 +1,22 @@
+var map = function() {
+
+    var sourceOccs = this.value.s_occs;
+    var targetOccs = this.value.t_occs;
+    var edgeOccs = this.value.occs;
+
+    emit(new ObjectId, {
+        s: this._id.s,
+        t: this._id.t,
+        occs: edgeOccs,
+        dice: 2 * edgeOccs / (sourceOccs + targetOccs),
+        jaccard: edgeOccs / (sourceOccs + targetOccs - edgeOccs),
+        cosine: edgeOccs / (Math.sqrt(sourceOccs * targetOccs))
+    });
+}
+
+var reduce = function(key, values) {
+    return values[0];
+}
+
+db.edges_tags.drop();
+db.mr_edges_tags.mapReduce(map, reduce, {out: {replace: "edges_tags", sharded: true}});
