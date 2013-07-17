ec80d377e1b3a6934f0d8414e5673398d11e6770
Sebastian Marr <marr@spreadshirt.net>
2013-07-17 13:59:21 +0200

    add M/R calculation of click graph measures

--- code/graph/clicks/measures.js
+++ code/graph/clicks/measures.js
@@ -1,3 +1,22 @@
-load('graph/measures.js')
+var map = function() {
 
-buildMeasures(db.click_graph_nodes, db.click_graph_edges);
+    var sourceOccs = this.value.s_occs;
+    var targetOccs = this.value.t_occs;
+    var edgeOccs = this.value.occs;
+
+    emit(new ObjectId, {
+        s: this._id.s,
+        t: this._id.t,
+        occs: edgeOccs,
+        dice: 2 * edgeOccs / (sourceOccs + targetOccs),
+        jaccard: edgeOccs / (sourceOccs + targetOccs - edgeOccs),
+        cosine: edgeOccs / (Math.sqrt(sourceOccs * targetOccs))
+    });
+}
+
+var reduce = function(key, values) {
+    return values[0];
+}
+
+db.graph_edges_clicks.drop();
+db.mr_click_graph_edges.mapReduce(map, reduce, {out: {replace: "graph_edges_clicks", sharded: true}});
