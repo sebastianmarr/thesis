99ad57fa1aa583e273d823a9fb6c21ec977077c7
Sebastian Marr <mail@sebastianmarr.de>
2013-07-24 11:44:25 +0200

    add integration of click edges

--- code/integration/clicks.js
+++ code/integration/clicks.js
@@ -1,6 +1,7 @@
 var clicktrackingDB = db.getSiblingDB('clicktracking');
 var graphDB =  db.getSiblingDB('graph');
 
+// nodes
 clicktrackingDB.graph_nodes_clicks.find().forEach(importClickNode);
 
 function importClickNode(node) {
@@ -16,3 +17,28 @@ function importClickNode(node) {
         {upsert: true}
     );
 }
+
+//edges
+function importClickEdge(edge) {
+
+    var source = clicktrackingDB.graph_nodes_clicks.findOne({_id: edge.value.s});
+    var target = clicktrackingDB.graph_nodes_clicks.findOne({_id: edge.value.t});
+
+    var integratedSource = graphDB.nodes.findOne({string: source.value.query, language: source.value.language});
+    var integratedTarget = graphDB.nodes.findOne({string: target.value.query, language: target.value.language});
+
+    var integratedEdge = {
+        source: integratedSource._id,
+        target: integratedTarget._id,
+        type: 'click-co-occurence',
+        occurences: edge.value.occs,
+        dice: edge.value.dice,
+        jaccard: edge.value.jaccard,
+        cosine: edge.value.cosine
+    };
+
+    graphDB.edges.insert(integratedEdge);
+
+}   
+
+clicktrackingDB.graph_edges_clicks.find().forEach(importClickEdge);
