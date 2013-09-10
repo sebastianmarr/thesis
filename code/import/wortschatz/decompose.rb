ddf15cacda6e1cd1442d1df19f8455fbbfb21a98
Sebastian Marr <mail@sebastianmarr.de>
2013-09-10 14:57:08 +0200

    add no timeout option on decompose

--- code/import/wortschatz/decompose.rb
+++ code/import/wortschatz/decompose.rb
@@ -35,19 +35,21 @@ db = mongo.db 'graph'
 nodes = db.collection 'nodes'
 edges = db.collection 'edges'
 
-nodes.find({}, fields: ['string', 'language']).each do |tag_node|
-
-    words = words(tag_node)     
-
-    words.each do |word|
-
-        # insert word node
-        word_node = {string: word, language: tag_node['language']}
-        existing = nodes.find_one(word_node)
-        word_id = existing ? existing['_id'] : nodes.insert(word_node)
-
-        # insert word edges
-        edges.insert({source: tag_node['_id'], target: word_id, type: "decomposition"})
-        edges.insert({source: word_id, target: tag_node['_id'], type: "composition"})
+nodes.find({}, fields: ['string', 'language'], timeout: false) do |cursor|
+    cursor.each do |tag_node|
+        
+        words = words(tag_node)
+        
+        words.each do |word|
+            
+            # insert word node
+            word_node = {string: word, language: tag_node['language']}
+            existing = nodes.find_one(word_node)
+            word_id = existing ? existing['_id'] : nodes.insert(word_node)
+            
+            # insert word edges
+            edges.insert({source: tag_node['_id'], target: word_id, type: "decomposition"})
+            edges.insert({source: word_id, target: tag_node['_id'], type: "composition"})
+        end
     end
 end
