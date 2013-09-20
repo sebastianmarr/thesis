09fe232c299fa537aa1fe4685eebcff87b3e4656
Sebastian Marr <mail@sebastianmarr.de>
2013-09-20 14:22:25 +0200

    improve wortschatz integration
    include origin instead of singleWord on new nodes

--- code/integration/wortschatz.js
+++ code/integration/wortschatz.js
@@ -1,22 +1,22 @@
 var graphDB =  db.getSiblingDB('graph');
 var wortschatzDB = db.getSiblingDB('wortschatz');
 
-var insertAndFind = function(word) {
+var insertAndFind = function(word, origin) {
     if (graphDB.nodes.count({string: word}) === 0) {
         graphDB.nodes.insert({
             string: word,
             language: "de",
-            singleWord: true
+            origin: origin
         });
     }
-    return graphDB.nodes.find({string: word, singleWord: true});
+    return graphDB.nodes.find({string: word});
 }
 
 // integrate wortschatz properties onto single words
-graphDB.nodes.ensureIndex({singleWord: true, string: true});
+graphDB.nodes.ensureIndex({string: true});
 wortschatzDB.mr_cleanup.find().forEach(function(word) {
     graphDB.nodes.update(
-        {singleWord: true, string: word._id},
+        {string: word._id},
         {$set: {wortschatzProperties: word.value}},
         {multi: true}
     );
@@ -24,8 +24,8 @@ wortschatzDB.mr_cleanup.find().forEach(function(word) {
 
 // integrate domain edges between single words
 wortschatzDB.mr_domain_edges.find().forEach(function(edge) {
-    graphDB.nodes.find({singleWord: true, string: edge._id.s}).forEach(function(sourceNode) {
-        graphDB.nodes.find({singleWord: true, string: edge._id.t}).forEach(function(targetNode) {
+    graphDB.nodes.find({string: edge._id.s}).forEach(function(sourceNode) {
+        graphDB.nodes.find({string: edge._id.t}).forEach(function(targetNode) {
             var newEdge = {
                 source: sourceNode._id,
                 target: targetNode._id,
@@ -55,25 +55,25 @@ graphDB.nodes.find({wortschatzProperties: {$exists: true}}).forEach(function(sou
     });
 
     synonyms.forEach(function(synonym) {
-        insertAndFind(synonym).forEach(function(targetNode) {
+        insertAndFind(synonym, "synonym").forEach(function(targetNode) {
             graphDB.edges.insert({source: sourceNode._id, target: targetNode._id, type: "synonym"});
         });
     });
 
     thesaurus.forEach(function(thesaurusWord) {
-        insertAndFind(thesaurusWord).forEach(function(targetNode) {
+        insertAndFind(thesaurusWord, "thesaurus").forEach(function(targetNode) {
             graphDB.edges.insert({source: sourceNode._id, target: targetNode._id, type: "thesaurus"});
         });
     });
 
-    wordforms.forEach(function(synonym) {
-        insertAndFind(synonym).forEach(function(targetNode) {
+    wordforms.forEach(function(wordform) {
+        insertAndFind(wordform, "wordform").forEach(function(targetNode) {
             graphDB.edges.insert({source: sourceNode._id, target: targetNode._id, type: "wordform"});
         });
     });
 
     baseforms.forEach(function(baseform) {
-        insertAndFind(baseform.word).forEach(function(targetNode) {
+        insertAndFind(baseform.word, "baseform").forEach(function(targetNode) {
             graphDB.edges.insert({source: sourceNode._id, target: targetNode._id, type: "baseform"});
         });
     });
